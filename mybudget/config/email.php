c2a136c1fb5a0202099b1b1edf88d3d38159e289
Kapil Sharma <kapil@kapilsharma.info>
2013-06-20 13:51:51 +0530

    Email config

---
+++ mybudget/config/email.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+$config = Array(
+		'protocol' => 'smtp',
+		'smtp_host' => 'ssl://smtp.googlemail.com',
+		'smtp_port' => 465,
+		'smtp_user' => 'your_email@gmail.com',
+		'smtp_pass' => 'your_gmail_password',
+		'mailtype' => 'html',
+		'charset' => 'utf-8',
+		'newline' => "\r\n"
+    );
